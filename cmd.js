22d31e3ba06356934240e192cc9e3e531c1d0452
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2022-07-08 01:17:38 +0800

    menghitung waktu eksekusi dan masalah useragent

--- cmd.js
+++ cmd.js
@@ -3,8 +3,11 @@ const puppeteer = require('puppeteer');
 const randomUseragent = require('random-useragent');
 
 (async () => {
+    console.time('timeExecute');
     const url = "https://www.tokopedia.com/search?st=product&q=laptop";
-    const randomAgent = randomUseragent.getRandom();
+    const randomAgent = randomUseragent.getRandom(function (ua) {
+        return (ua.osName === "Linux") && (ua.browserName === 'Firefox' || ua.browserName === 'Chrome');
+    });
     const browser = await puppeteer.launch({
         args: [
             '--no-sandbox',
@@ -14,7 +17,7 @@ const randomUseragent = require('random-useragent');
     const context = await browser.createIncognitoBrowserContext();//mode penyamaran
     const page = await context.newPage();//membuat tab baru
     await page.setJavaScriptEnabled(true);//aktifkan javascript
-    await page.setUserAgent(randomAgent);//setting user agent
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36");//setting user agent
     await page.goto(url, { waituntil: 'domcontentloaded', timeout: 0 });//tunggu proses dom/load pagenya selesai
     const body = await page.evaluate(() => {
         return document.querySelector('body').innerHTML;
@@ -22,6 +25,9 @@ const randomUseragent = require('random-useragent');
 
     const $ = cheerio.load(body);
     const listItems = $('[data-testid="master-product-card"]');
+    if(listItems.length <= 0){
+        console.log(body);
+    }
 
     var resulst = [];
     listItems.each(function (idx, el) {
@@ -42,4 +48,5 @@ const randomUseragent = require('random-useragent');
 
 
     await browser.close();//close browser puppeteer jika sudah selesai
+    console.timeEnd('timeExecute')
 })();
